c9c51b1a42d55f2692bf39d8ca5d57f33c236ab1
DriDri1308 <dada2011livia23@gmail.com>
2024-10-19 19:10:16 -0300

    atualizar

--- webpack.config.js
+++ webpack.config.js
@@ -28,5 +28,17 @@ module.exports = {
   },
   resolve: {
     extensions: ['.js', '.jsx'], // Extensões reconhecidas
+    fallback: {
+      fs: false, // Se não precisa do fs, defina como false
+      http: require.resolve('stream-http'), // Adiciona o polyfill para o módulo http
+      https: require.resolve('https-browserify'), // Adiciona o polyfill para o módulo https
+      stream: require.resolve('stream-browserify'), // Adiciona o polyfill para o módulo stream
+      buffer: require.resolve('buffer/'), // Adiciona o polyfill para o módulo buffer
+      process: require.resolve('process/browser'), // Adiciona o polyfill para o módulo process
+      crypto: require.resolve('crypto-browserify'), // Adiciona o polyfill para o módulo crypto
+      querystring: require.resolve('querystring-es3'), // Adiciona o polyfill para o módulo querystring
+      path: require.resolve('path-browserify'), // Adiciona o polyfill para o módulo path
+      url: require.resolve('url'), // Adiciona o polyfill para o módulo url
+    },
   },
 };
